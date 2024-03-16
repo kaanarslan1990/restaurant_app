d42be094bca8f87c38ff79e06d8fad9585f84ee5
okarslan <okarslan@dabnet.com.tr>
2024-03-17 01:18:35 +0300

    axios setting

---
+++ restoran-khan/hooks/useResault.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import yelp from "../api/yelp";
+
+export default ()=> {
+
+const searchApi = async (searchTerm) => {
+    await yelp.get('/search',{
+        params:{
+            limit:50,
+            term:searchTerm,
+            location:'İstanbul'
+        }
+    })
+
+}
+
+    useEffect(()=> {
+        searchApi('Toast');
+
+    }, []);
+
+    return [searchApi]
+}
