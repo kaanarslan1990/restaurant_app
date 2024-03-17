fc5eead7a3a8fe05013339b8555659ecee2b0bed
okarslan <okarslan@dabnet.com.tr>
2024-03-18 00:28:47 +0300

    refactor

--- restoran-khan/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseUrl: "https://api.yelp.com/v3/businesses",
-  headers: {
-    Authorization:
-      "Bearer 0nqqMWxASfxglzDFHAA8Kc-HRK2q-RCt1b_C70nruzybz6CuZdJWZmwv2pkvEpv3y7lt0zNdJoR-3VM-gi2GPFJidDFxUKuwrR3k7yEaRbUy6fcIYD_NQvJympXtZXYx",
-  },
-});
