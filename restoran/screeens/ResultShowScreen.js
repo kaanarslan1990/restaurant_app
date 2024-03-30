a9eee075078798493ad500c3fdcea5c061036101
okarslan <okarslan@dabnet.com.tr>
2024-03-30 15:27:51 +0300

    colse or open

--- restoran/screeens/ResultShowScreen.js
+++ restoran/screeens/ResultShowScreen.js
@@ -1,6 +1,8 @@
 import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import yelp from "../api/yelp";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ResultShowScreen({ route }) {
   const [result, setResult] = useState(null);
@@ -18,18 +20,41 @@ export default function ResultShowScreen({ route }) {
   }
   return (
     <View>
-      <Text>{result.name}</Text>
-      <Text>{result.phone}</Text>
+      <Text style={styles.title}>{result.name}</Text>
+      <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <AntDesign name="closecircleo" size={30} color="black" />
+        ) : (
+          <MaterialIcons name="delivery-dining" size={30} color="black" />
+        )}
+      </View>
       <FlatList
         data={result.photos}
         renderItem={({ item }) => {
-          return (
-            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
-          );
+          return <Image style={styles.image} source={{ uri: item }} />;
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 180,
+    margin: 10,
+    borderRadius: 15,
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 25,
+    marginVertical: 10,
+  },
+  phone: {
+    alignSelf: "center",
+    fontSize: 20,
+  },
+  icon:{
+    alignSelf:'center'
+  }
+});
